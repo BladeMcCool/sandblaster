3ad734b6a9df34d5f61367dcb2cafd83f5364c92
Chris Hagglund <chris@convertstar.com>
2017-07-10 13:42:59 -0700

    got a list of proxy pages to scrape from first hinky page.

---
+++ scrapey.go
@@ -0,0 +1,158 @@
+package main
+
+import (
+	"fmt"
+	"github.com/PuerkitoBio/goquery"
+	"github.com/spaolacci/murmur3"
+	_ "io"
+	"io/ioutil"
+	"log"
+	"os"
+	"path/filepath"
+	"regexp"
+	"strconv"
+	"time"
+)
+
+const cacheDir string = "cache"
+
+const maxCacheAge int = 1200
+
+// const maxCacheAge int = 300
+
+func scrapey() {
+	fmt.Printf("Yeah herely.\n")
+	// startUrl := "https://example.com"
+	// doc, err := goquery.NewDocument(startUrl)
+	// if err != nil {
+	// 	log.Fatal(err)
+	// }
+	// fmt.Println(doc.HTML())
+
+	// doc.Find("a.menu").Each(func(i int, item *goquery.Selection) {
+	// 	fmt.Println(i, item.Text())
+	// })
+
+	hinkyDink()
+
+	//read the first page, url set here.
+	//check our local cache first, if page is not in it or is old, get fresh.
+	//get a list of pages to get. or start at the first one and keep going until they are all gotten.
+	//on each page, extract the proxy info
+	//save the proxy info somewhere. for now, a text file is fine.
+
+}
+
+func hinkyDink() {
+	hinkyBase := "http://www.mrhinkydink.com/"
+	startUrl := hinkyBase + "proxies.htm"
+	firstPage := getDoc(startUrl)
+	if firstPage == nil {
+		log.Fatalf("no first page of hinky dink. fatality\n")
+	}
+
+	proxyPageRe := regexp.MustCompile(`Proxies page (\d+)`)
+	// if err != nil {
+	// 	log.Fatalf("regexp err: %s\n", err.Error())
+	// }
+	firstPage.Find("a.menu").Each(func(i int, item *goquery.Selection) {
+		linkPath, _ := item.Attr("href")
+		linkText := item.Text()
+		matchInfo := proxyPageRe.FindSubmatch([]byte(linkText))
+		if len(matchInfo) != 2 {
+			return
+		}
+		fullLink := hinkyBase + linkPath
+		pageNum, _ := strconv.Atoi(string(matchInfo[1]))
+		if pageNum <= 1 {
+			return
+		}
+		log.Printf("url for page %d is %s\n", pageNum, fullLink)
+
+		// for ind, fucker := range matchInfo {
+		// 	fmt.Printf("something: %d %s\n", ind, fucker)
+		// }
+		// // fmt.Printf("something: % #v\n", pretty.Formatter(something))
+		// fmt.Println(i, linkText, linkPath, )
+	})
+
+}
+
+func getDoc(url string) *goquery.Document {
+	localCacheFileName := urlToFilename(url)
+	isCached := checkLocalCache(localCacheFileName)
+
+	if isCached {
+		log.Printf("Should read from the local cached copy.\n")
+		return docFromCache(localCacheFileName)
+	}
+	doc := docFromUrl(url)
+	cacheDoc(doc, url)
+	return doc
+}
+
+func urlToFilename(url string) string {
+	//maybe we should hash it.
+	//for now lets log the filename conversion too so we can tell what the hashes were
+	localFilename := fmt.Sprintf("%x.cached", murmur3.Sum64([]byte(url)))
+	fmt.Printf("DBG url hash %s from %s\n", localFilename, url)
+	return localFilename
+}
+
+func checkLocalCache(filename string) bool {
+	//if filename exists in local cache dir, then return a io reader to read the file contents.
+	cachePath := filepath.Join(".", cacheDir)
+	if _, err := os.Stat(cachePath); os.IsNotExist(err) {
+		log.Printf("Created cache directory at %s\n", cachePath)
+		os.Mkdir(cachePath, os.ModePerm)
+	}
+
+	//file not exist?
+	filePath := filepath.Join(cachePath, filename)
+	log.Printf("Checking if cache file %s exists\n", filePath)
+	fileStats, err := os.Stat(filePath)
+	// var err error
+	if err != nil && os.IsNotExist(err) {
+		log.Printf("file %s does not exist\n", filePath)
+		return false
+	}
+
+	// log.Printf("File stats: %# v\n", pretty.Formatter(fileStats))
+	fileAgeSec := time.Since(fileStats.ModTime()).Seconds()
+	log.Printf("File age sec %f\n", fileAgeSec)
+	if int(fileAgeSec) > maxCacheAge {
+		log.Printf("File age sec %f is too old\n", fileAgeSec)
+		return false
+	}
+
+	return true
+}
+
+func docFromCache(filename string) *goquery.Document {
+	fpath := filepath.Join(".", cacheDir, filename)
+	log.Printf("docFromCache got io.reader for %s\n", fpath)
+	reader, _ := os.Open(fpath)
+	doc, err := goquery.NewDocumentFromReader(reader)
+	if err != nil {
+		log.Fatal(err)
+	}
+	return doc
+}
+func docFromUrl(url string) *goquery.Document {
+	log.Printf("Loading document from url %s\n", url)
+	doc, err := goquery.NewDocument(url)
+	if err != nil {
+		log.Fatal(err)
+	}
+	return doc
+}
+func cacheDoc(doc *goquery.Document, url string) {
+	fn := urlToFilename(url)
+	fpath := filepath.Join(".", cacheDir, fn)
+	html, _ := doc.Html()
+	err := ioutil.WriteFile(fpath, []byte(html), os.ModePerm)
+	if err != nil {
+		log.Fatalf("Failed to write file to cache at %s.\n", fpath)
+	}
+	log.Printf("Cached url %s to local cache file %s\n", url, fn)
+}
